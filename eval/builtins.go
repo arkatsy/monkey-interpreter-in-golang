457ea473a2e96fdd8b56c0967eae0b54639a1f4c
Argyris <arkatsy@gmail.com>
2023-08-04 18:32:47 +0300

    implement builtin print

--- eval/builtins.go
+++ eval/builtins.go
@@ -1,6 +1,9 @@
 package eval
 
-import "monkey-go/object"
+import (
+	"fmt"
+	"monkey-go/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -101,6 +104,16 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+
+	"print": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
